7acc7e1cb577b59067532f72cf26c773699416da
GuilhermeSilva06 <up202404271@up.pt>
2025-02-21 16:31:35 +0000

    Exercício 1 e 2 da ficha 1 de Prog2, resolvido e funcional.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Cilindro cilindro = new Cilindro();
+        cilindro.setRaio(2);
+        cilindro.setAltura(3);
+
+        Cilindro cil2 = new Cilindro();
+        cil2.setRaio(3);
+        cil2.setAltura(4);
+
+        System.out.println("Volume: " + cilindro.volume());
+
+        System.out.println("Volume: " + cil2.volume());
+    }
+}
